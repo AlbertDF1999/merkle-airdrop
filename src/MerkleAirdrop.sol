53d1d73bcb6273aaf93adb9275df0abee07bcf8c
AlbertoCastroF <alberto.castro1987@gmail.com>
2025-12-19 11:55:22 -0500

    finished merkleairdrop and bagletoken contracts

---
+++ src/MerkleAirdrop.sol
@@ -0,0 +1,42 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.24;
+
+import {IERC20, SafeERC20} from "lib/openzeppelin-contracts/contracts/token/ERC20/utils/SafeERC20.sol";
+import {MerkleProof} from "lib/openzeppelin-contracts/contracts/utils/cryptography/MerkleProof.sol";
+
+contract MerkleAirdrop {
+    using SafeERC20 for IERC20;
+
+    error MerkleAirdrop__InvalidProof();
+    error MerkleAirdrop__AlreadyClaimed();
+
+    //some list of addresses
+    //allow someone in the list to mint some tokens
+    address[] claimers;
+    mapping(address claimer => bool claimed) private s_hasClaimed;
+    bytes32 private immutable i_merkleRoot;
+    IERC20 private immutable i_airdropToken;
+
+    event Claim(address account, uint256 amount);
+
+    //merkle proofs allow us to prove that a piece of data in inside a groupt of data
+    constructor(bytes32 merkleRoot, IERC20 airdropToken) {
+        i_merkleRoot = merkleRoot;
+        i_airdropToken = airdropToken;
+    }
+
+    function claim(address account, uint256 amount, bytes32[] calldata merkleProof) external {
+        if (s_hasClaimed[account]) {
+            revert MerkleAirdrop__AlreadyClaimed();
+        }
+        //calculate using the account, amount and the hash -> leaf node
+        bytes32 leaf = keccak256(bytes.concat(keccak256(abi.encode(account, amount))));
+        if (!MerkleProof.verify(merkleProof, i_merkleRoot, leaf)) {
+            revert MerkleAirdrop__InvalidProof();
+        }
+        s_hasClaimed[account] = true;
+        emit Claim(account, amount);
+        i_airdropToken.safeTransfer(account, amount);
+    }
+}
